2b81b184809251a880e6634006a13f26875d33b3
Jigsaw <matheus_jigsaw@outlook.com>
2024-08-25 21:14:28 -0300

    Atualizando

--- dao/PessoaDao.java
+++ dao/PessoaDao.java
@@ -1,2 +1,91 @@
-package dao;public class PessoaDao {
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PessoaDao {
+
+    private File arquivo;
+
+    public PessoaDao(){
+        this.arquivo= new File("Pessoas.ser");
+
+        if(!arquivo.exists()){
+            try {
+                arquivo.createNewFile();
+            }catch (IOException e){
+                System.out.println("Falha ao criar arquivo Pessoas.ser.");
+            }
+        }
+
+    }
+
+    //Pegando lista de pessoas
+    public Set<Pessoa> getPessoas(){
+        if(arquivo.length() > 0){
+            try {
+                FileInputStream inputArquivo = new FileInputStream(arquivo);
+                ObjectInputStream objectInputStream = new ObjectInputStream(inputArquivo);
+                Set<Pessoa> pessoas = (Set<Pessoa>) objectInputStream.readObject();
+                return pessoas;
+            }catch (FileNotFoundException e){
+                System.out.println("Arquivo não localizado.");
+            }catch (IOException e){
+                System.out.println("Falha ao escrever no arquivo.");
+            }catch (ClassNotFoundException e){
+                System.out.println("Falha ao ler arquivo.");
+            }
+        }
+        return new HashSet<>();
+    }
+
+    //Salvando pessoas na lista
+    public boolean salvarPessoa(Pessoa pessoa){
+        Set<Pessoa> pessoas = getPessoas();
+        if(pessoas.add(pessoa)){
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(arquivo);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                objectOutputStream.writeObject(pessoas);
+                return true;
+            }catch (FileNotFoundException e){
+                System.out.println("Arquivo não encontrado.");
+            }catch (IOException e){
+                System.out.println("Falha ao escrever no arquivo.");
+            }
+        }
+        return false;
+    }
+
+    //Deletando uam pessoa da lista
+    public boolean deletarPessoa(Pessoa pessoa){
+        Set<Pessoa> pessoas = getPessoas();
+        if(pessoas.remove(pessoa)){
+            try {
+                FileOutputStream fileOutputStream = new FileOutputStream(arquivo);
+                ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+                objectOutputStream.writeObject(pessoas);
+                return true;
+            }catch (FileNotFoundException e){
+                System.out.println("Arquivo não encontrado.");
+            }catch (IOException e){
+                System.out.println("Falha ao escrever no arquivo.");
+            }
+        }
+        return false;
+    }
+
+
+    //Deletando pessoa por email
+    public boolean deletarPorEmail(String email){
+        Set<Pessoa> pessoas = getPessoas();
+        for(Pessoa pessoa : pessoas){
+            if(pessoa.getEmail().equals(email)){
+                System.out.println("Pessoa deletada com sucesso.");
+                deletarPessoa(pessoa);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- view/Main.java
+++ view/Main.java
@@ -1,2 +1,63 @@
-package view;public class Main {
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner;
+    public static void main(String[] args){
+
+        PessoaDao pessoas = new PessoaDao();
+        scanner = new Scanner(System.in);
+        int resposta = 0;
+
+        while(resposta != 4){
+            System.out.println("---- MENU ----");
+            System.out.println("1 - Salvar Pessoa");
+            System.out.println("2 - Listar Cadastros de Pessoas");
+            System.out.println("3 - Deletar por e-mail");
+            System.out.println("4 - Sair");
+
+            resposta = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (resposta){
+                case 1:
+                    Pessoa novaPessoa = cadastrarPessoa();
+                    pessoas.salvarPessoa(novaPessoa);
+                    break;
+                case 2:
+                    System.out.println("--- Lista de Pessoas Cadastradas ---");
+
+                    for (Pessoa pessoa : pessoas.getPessoas()){
+                        System.out.println("Nome: " + pessoa.getNome());
+                        System.out.println("E-mail: " + pessoa.getEmail());
+                        System.out.println("\n");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Informe o e-mail da pessoa a ser deletada: ");
+                    String email = scanner.nextLine();
+                    pessoas.deletarPorEmail(email);
+                    break;
+                case 4:
+                    System.out.println("Saindo do sistema!");
+                    break;
+                default:
+                    System.out.println("Escolha uma opção válida!");
+            }
+
+        }
+
+        scanner.close();
+    }
+
+
+    private static Pessoa cadastrarPessoa(){
+        System.out.println("Informe o nome da pessoa a ser cadastrada: ");
+        String nomePessoa = scanner.nextLine();
+        System.out.println("Informe o e-mail da pessoa a ser adicionada");
+        String emailPessoa = scanner.nextLine();
+        Pessoa novaPessoa = new Pessoa(nomePessoa,emailPessoa);
+        return novaPessoa;
+    }
+
 }
